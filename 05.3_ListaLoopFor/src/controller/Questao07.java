38cfbf9ae5db04e4a0bb0a0aab180f7fa3014f20
ROSADEV <ROSADEV@ROSADEV>
2024-04-10 10:04:54 -0300

    questao-07

---
+++ 05.3_ListaLoopFor/src/controller/Questao07.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Questao07 {
+	public static void main(String[] args) {
+		for (int l = 1; l <=10; l++) {
+			for (int c = 1; c <= 10; c++) {
+				System.out.print(l + " - " + c + "\t");
+			}
+		System.out.println();
+		}
+	}
+}
